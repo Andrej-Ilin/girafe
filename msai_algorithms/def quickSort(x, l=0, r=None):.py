7c7a23d86a3b875bed9c2ad32a48f18ea677fa01
Andrei <andrej_ilin7@mail.ru>
2024-10-09 14:47:01 +0300

    quickSort

--- msai_algorithms/def quickSort(x, l=0, r=None):.py
+++ msai_algorithms/def quickSort(x, l=0, r=None):.py
@@ -1,19 +1,24 @@
 import random
 
-def quickSort(x, l=0, r=None):
+with open('./girafe/msai_algorithms/file.txt', 'r') as f:
+    data = f.read().strip().split('\n')
+
+def quickSort(x, l=0, r=None, reverse=False):
     if r is None:
         r = len(x)  # Adjust this to include the last element in the range
-    if (r - l) > 1: # Ensure that there are at least two elements to sort
-        pivot = x[random.randint(l, r - 1)]
-        less = [i for i in x[l:r] if i < pivot]  # Elements less than pivot
-        equal = [i for i in x[l:r] if i == pivot]  # Elements equal to pivot
-        greater = [i for i in x[l:r] if i > pivot]  # Elements greater than pivot
+    if (r - l) > 1:  # Ensure that there are at least two elements to sort
+        pivot = x[random.randint(l, r - 1)][1]  # Use the value of the tuple as the pivot
+        less = [i for i in x[l:r] if i[1] < pivot]  # Elements less than pivot
+        equal = [i for i in x[l:r] if i[1] == pivot]  # Elements equal to pivot
+        greater = [i for i in x[l:r] if i[1] > pivot]  # Elements greater than pivot
         
         # Recursively sort the less and greater parts and combine them
         return quickSort(less, 0, len(less)) + equal + quickSort(greater, 0, len(greater))
     return x  # Return the sorted elements
 
-# Example usage
-arr = [3, 6, 8, 10, 1, 2, 4, 5, 7, 9]
-sorted_arr = quickSort(arr)
-print(sorted_arr)
+N = int(data[0])
+print(N)
+print(list(map(int, data[1].split())))
+x = list(map(int, data[1].split()))
+
+print(quickSort(x))
---
+++ msai_algorithms/writer_txt.py
@@ -0,0 +1,2 @@
+with open('./girafe/msai_algorithms/write.txt', 'w') as f:
+    f.write(input())
