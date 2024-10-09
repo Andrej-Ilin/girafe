0966fb9c9e7747e8e4e3579590eca213368bef0d
Andrei <andrej_ilin7@mail.ru>
2024-10-09 14:47:01 +0300

    quickSort

--- msai_algorithms/B. Standings.py
+++ msai_algorithms/B. Standings.py
@@ -1,42 +1,50 @@
-import sys
 import random
-with open('./girafe/msai_algorithms/file.txt', 'r') as f:
-    data = f.read().strip().split('\n')
-# print(data)
 
-N = int(data[0])
 
-student = {}
+def partition(x, l, r, pivot):
+    """
+    :param x: Source array (list)
+    :param l: left border of partitioning range (int)
+    :param r: right border (not included) of partitioning range (int)
+    :param pivot: pivot element to divide array (any item from x[l, r)).
+    :return: il, ir -- desired partition
+    This function should reorder elements of x within [l, r) region
+    in the way, these conditions are true:
+    x[l:il] < pivot
+    x[il:ir] == pivot
+    x[ir:r] > pivot
+    """
+    # Your code goes here
+    il = l  # index for less than pivot
+    ir = l  # index for equal to pivot
 
-def quickSort(x, l=0, r=None):
+    for i in range(l, r):
+        if x[i] < pivot:
+            x[il], x[i] = x[i], x[il]  # swap to the left
+            if il < ir:
+                x[ir], x[i] = x[i], x[ir]  # swap equal to pivot after il
+            il += 1
+            ir += 1
+        elif x[i] == pivot:
+            x[ir], x[i] = x[i], x[ir]  # swap equal to pivot
+            ir += 1
+    
+    return il, ir
+
+
+
+def qsort(x, l=0, r=None):
     if r is None:
-        r = len(x)  # Adjust this to include the last element in the range
-    if (r - l) > 1: # Ensure that there are at least two elements to sort
+        r = len(x)
+    if (r - l) > 1:
         pivot = x[random.randint(l, r - 1)]
-        less = [i for i in x[l:r] if i < pivot]  # Elements less than pivot
-        equal = [i for i in x[l:r] if i == pivot]  # Elements equal to pivot
-        greater = [i for i in x[l:r] if i > pivot]  # Elements greater than pivot
-        
-        # Recursively sort the less and greater parts and combine them
-        return quickSort(less, 0, len(less)) + equal + quickSort(greater, 0, len(greater))
-    return x  # Return the sorted elements
+        il, ir = partition(x, l, r, pivot)
+        qsort(x, l, il)
+        qsort(x, ir, r)
 
-# read of the data
-list_score = []
 
-for i in range(1, N + 1):
-    line = data[i].split()
-    # print(line)
-    id = int(line[0])
-    score = int(line[1])
-    list_score.append(score)
-    nickname = line[2]
-    student[score] = [id, nickname]
-# print(student.get())
-# print(quickSort(list_score)[::-1])
-sort_score = quickSort(list_score)[::-1]
-top_3_nickname = []
-
-for i in sort_score[:3]:
-    top_3_nickname.append(student.get(i)[1])
-print(top_3_nickname)
+if __name__ == '__main__':
+    N = int(input())
+    x = list(map(int, input().split()))
+    qsort(x)
+    print(' '.join(map(str, x)))
